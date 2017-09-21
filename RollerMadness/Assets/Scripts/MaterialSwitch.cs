1b336fe45afc938b8156c40a99f3f4490cb10021
nickzinkler <nikita-tsinkler@yandex.ru>
2017-09-21 14:23:20 +0800

    Games

---
+++ RollerMadness/Assets/Scripts/MaterialSwitch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MaterialSwitch : MonoBehaviour {
+
+    public Material[] materials = new Material[3];
+    Health health;
+    Renderer rend;
+    int hp;
+    public Text text;
+
+    // Use this for initialization
+    void Start () {
+        health = GetComponent<Health>();
+        rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hp = (int)health.healthPoints - 1;
+        if (hp >= 0 && hp < 3)
+        {
+            rend.material = materials[(int)health.healthPoints - 1];
+        }
+        text.text = "Lives: " + health.healthPoints;
+	}
+}
---
+++ RollerMadness/Assets/Scripts/RandomPitch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPitch : MonoBehaviour {
+
+    AudioSource audio;
+    public float MinPitch, MaxPitch;
+
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+        audio.pitch = Random.Range(MinPitch, MaxPitch);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
